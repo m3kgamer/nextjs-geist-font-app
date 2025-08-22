ec8b62792e56024f98fa90431617940b2335e006
m3kgamer <149871776+m3kgamer@users.noreply.github.com>
2025-08-22 13:26:02 +0100

    Add .next/static/chunks/src_app_contact_page_tsx_a993527f._.js 1755865562370

---
+++ .next/static/chunks/src_app_contact_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_84cd7f6b._.js",
+  "static/chunks/src_16e53790._.js"
+],
+    source: "dynamic"
+});
